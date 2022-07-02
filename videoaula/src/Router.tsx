fbc0da8ff510aca4a2d5d79696a2a9429bfea573
Gil C S <teksyseletronica@terra.com.br>
2022-07-01 22:14:13 -0300

    Plataforma cadastro

--- videoaula/src/Router.tsx
+++ videoaula/src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
 import { Home } from "./pages/Home";
+import { Subscribe } from "./pages/subscribe";
 
 export function Router() {
     return (
         <Routes>
+            <Route path="/subscribe" element={<Subscribe />} />
             <Route path="/home" element={<Home />} />
             <Route path="/event" element={<Event />} />
             <Route path="/event/lessons/:slug" element={<Event />} />
